b72df4c63cf17e7d61035f7c9d0a580bdfe1da14
ZarKljh <zarkljh@gmail.com>
2025-07-23 15:29:45 +0900

    변수 새로고침 증가

--- src/main/java/com/std/std/HomeController.java
+++ src/main/java/com/std/std/HomeController.java
@@ -10,16 +10,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // 아래에 있는 HomeController 는 컨트롤러이다.
 @Controller
 public class HomeController {
+    int request = 0;
     //@GetMapping의 의미는?
     // 개발자가 스프릥부트에게 알려주는 것
     // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘.
     // 기본적으로 요청은 localhost:8080/home/main 이렇게 들어온다
-    @GetMapping("/home/main")
+    @GetMapping("/home/increase")
     // 요청이 들어오면 이 아래에 적혀있는 메서드가 실행된다
     @ResponseBody
-    //@ResponseBody MVC모델에서 View 모델에게 출력을 전가하지 않고, 직접 출력하겠다는 의미이다
+    //@ResponseBody MVC모델에서 View 모델에게 출력을 요청하지 않고, 직접 출력하겠다는 의미이다
     public String ShowMain(){
-        return "Hello World";
-    }
 
+        boolean run = true;
+
+        while(run){
+            if(request < 3){
+                request++;
+                return "응답 : " + (request);
+            } else {
+                run = false;
+            }
+        }
+        return "Hello!!";
+    }
 }
