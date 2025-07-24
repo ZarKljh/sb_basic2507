f8c2e3eca6110586ff137dbf36738eb3bc25814b
ZarKljh <zarkljh@gmail.com>
2025-07-24 15:07:50 +0900

    ãremove Add

--- src/main/java/com/std/std/PersonController.java
+++ src/main/java/com/std/std/PersonController.java
@@ -32,6 +32,29 @@ public class PersonController {
         return String.format("%d번째 사람 %s이(가) 추가되었습니다", person.getId(), person.getName());
     }
 
+    @GetMapping("/person/remove")
+    @ResponseBody
+    public String removePerson(@RequestParam("id") int id){
+
+        Person person = null;
+        String result = "";
+        for(Person p : personList){
+            if(p.getId() == id){
+                person = p;
+            }
+        }
+        if(person == null){
+            result = String.format("%d번 사람은 존재하지 않습니다",id);
+        } else {
+            personList.remove(person);
+            result = String.format("%d번 사람이 삭제되었습니다",person.getId());
+        }
+
+
+        return result;
+    }
+
+
     @GetMapping("/person/people")
     @ResponseBody
     public List<Person> getPersonList(){
